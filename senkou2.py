d4f2e1ef0d2f2004be07bbc9a75510f64d142ec3
naoen <s1611486@u.tsukuba.ac.jp>
2018-05-28 18:16:13 +0900

    プログラム変更

--- senkou2.py
+++ senkou2.py
@@ -1,26 +1,12 @@
 import cv2
 
-# 分類器ディレクトリ(以下から取得)
-# https://github.com/opencv/opencv/blob/master/data/haarcascades/
-# https://github.com/opencv/opencv_contrib/blob/master/modules/face/data/cascades/
-
 cascade_path = r'C:\Users\lamp\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml'
 
-# 他のモデルファイル(参考)
-#cascade_path = "./models/haarcascade_frontalface_alt.xml"
-#cascade_path = "./models/haarcascade_frontalface_alt2.xml"
-#cascade_path = "./models/haarcascade_frontalface_alt_tree.xml"
-#cascade_path = "./models/haarcascade_profileface.xml"
-#cascade_path = "./models/haarcascade_mcs_nose.xml"
 
 # 使用ファイルと入出力ディレクトリ
 image_file = "human2.jpg"
 image_path = "images/" + image_file
 output_path = "images/aa/" + image_file
-
-# ディレクトリ確認用(うまく行かなかった時用)
-#import os
-#print(os.path.exists(image_path))
 
 #ファイル読み込み
 image = cv2.imread(image_path)
@@ -31,13 +17,6 @@ image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 #カスケード分類器の特徴量を取得する
 cascade = cv2.CascadeClassifier(cascade_path)
 
-#物体認識（顔認識）の実行
-#image – CV_8U 型の行列．ここに格納されている画像中から物体が検出されます
-#objects – 矩形を要素とするベクトル．それぞれの矩形は，検出した物体を含みます
-#scaleFactor – 各画像スケールにおける縮小量を表します
-#minNeighbors – 物体候補となる矩形は，最低でもこの数だけの近傍矩形を含む必要があります
-#flags – このパラメータは，新しいカスケードでは利用されません．古いカスケードに対しては，cvHaarDetectObjects 関数の場合と同じ意味を持ちます
-#minSize – 物体が取り得る最小サイズ．これよりも小さい物体は無視されます
 facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
 
 #print(facerect)
